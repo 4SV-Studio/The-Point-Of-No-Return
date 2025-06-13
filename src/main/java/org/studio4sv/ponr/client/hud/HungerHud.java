fae7975a2af0e9efdc1756a027e0aae012f2e649
Lemonnik6484 <lemongamer6484@gmail.com>
2025-06-13 22:19:06 +0200

    fix hunger texture

--- src/main/java/org/studio4sv/ponr/client/hud/HungerHud.java
+++ src/main/java/org/studio4sv/ponr/client/hud/HungerHud.java
@@ -1,6 +1,7 @@
 package org.studio4sv.ponr.client.hud;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
@@ -15,20 +16,29 @@ public class HungerHud {
         currentHungerStage = Math.max(1, Math.min(6, stage));
     }
 
-    public static final IGuiOverlay HUD_HUNGER = (gui, poseStack, partialTick, screenWidth, screenHeight) -> {
-
+    public static final IGuiOverlay HUD_HUNGER = (gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, HUNGER_TEXTURE);
 
         int padding = 10;
 
-        int width = 35;
-        int height = 35;
-        int textureSize = 26;
-        int x = screenWidth - width - padding;
-        int y = height + (padding * 2);
+        int scaledWidth = 35;
+        int scaledHeight = 35;
 
-        poseStack.blit(HUNGER_TEXTURE, x, y, 0, textureSize * (currentHungerStage - 1), textureSize, textureSize, textureSize, textureSize * 6); // texture, x, y, u, v, width, height, textureWidth, textureHeight
+        int textureSize = 26;
+        int x = screenWidth - scaledWidth - padding;
+        int y = scaledHeight + (padding * 2);
+
+        PoseStack poseStack = guiGraphics.pose();
+
+        poseStack.pushPose();
+        poseStack.translate(x, y, 0);
+        float scale = 35.0f / 26.0f;
+        poseStack.scale(scale, scale, 1.0f);
+
+        guiGraphics.blit(HUNGER_TEXTURE, 0, 0, 0, textureSize * (currentHungerStage - 1), textureSize, textureSize, textureSize, textureSize * 6);
+
+        poseStack.popPose();
     };
 }
