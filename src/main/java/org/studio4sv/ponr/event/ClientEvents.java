b30f664ff068bbea4e0c7d467585194a263b949a
Lemonnik6484 <lemongamer6484@gmail.com>
2025-06-15 00:46:00 +0200

    better class names

--- src/main/java/org/studio4sv/ponr/event/ClientEvents.java
+++ src/main/java/org/studio4sv/ponr/event/ClientEvents.java
@@ -1,20 +1,35 @@
 package org.studio4sv.ponr.event;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.event.entity.player.PlayerXpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.studio4sv.ponr.PONR;
-import org.studio4sv.ponr.client.hud.HungerHud;
-import org.studio4sv.ponr.client.hud.StaminaHud;
+import org.studio4sv.ponr.client.hud.XpHud;
+import org.studio4sv.ponr.util.xpConverter;
 
+@Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientEvents {
-    @Mod.EventBusSubscriber(modid = PONR.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModBusEvents {
-        @SubscribeEvent
-        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event) {
-            event.registerAboveAll("ponr_hunger", HungerHud.HUD_HUNGER);
-            event.registerAboveAll("ponr_stamina", StaminaHud.HUD_STAMINA);
-        }
+
+    @SubscribeEvent
+    public static void disableHud(RenderGuiOverlayEvent.Pre event) {
+        if (event.getOverlay() == VanillaGuiOverlay.FOOD_LEVEL.type())
+            event.setCanceled(true);
+        if (event.getOverlay() == VanillaGuiOverlay.EXPERIENCE_BAR.type())
+            event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public static void onXpChange(PlayerXpEvent.XpChange event) {
+        Player player = event.getEntity();
+
+        int level = player.experienceLevel;
+        float progress = player.experienceProgress;
+
+        int totalXP = xpConverter.calculate(level, progress);
+
+        XpHud.setPoints(totalXP);
     }
 }
