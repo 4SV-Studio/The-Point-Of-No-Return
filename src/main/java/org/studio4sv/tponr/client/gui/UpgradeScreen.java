16f7597d61d579288d4758cd7d7d221ef5352efe
Lemonnik6484 <lemongamer6484@gmail.com>
2025-06-23 00:51:46 +0200

    Current values display

--- src/main/java/org/studio4sv/tponr/client/gui/UpgradeScreen.java
+++ src/main/java/org/studio4sv/tponr/client/gui/UpgradeScreen.java
@@ -4,8 +4,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import org.studio4sv.tponr.TPONR;
 import org.studio4sv.tponr.client.ClientAttributesData;
+import org.studio4sv.tponr.mechanics.stamina.PlayerStaminaProvider;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class UpgradeScreen extends Screen {
     public UpgradeScreen() {
@@ -14,7 +19,6 @@ public class UpgradeScreen extends Screen {
 
     @Override
     protected void init() {
-
         super.init();
     }
 
@@ -22,24 +26,70 @@ public class UpgradeScreen extends Screen {
     public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
         Player player = Minecraft.getInstance().player;
 
+        int upgradeScreenWidth = 286;
+        int upgradeScreenHeight = 168;
+        pGuiGraphics.blit(ResourceLocation.fromNamespaceAndPath(TPONR.MOD_ID, "textures/gui/upgrade_screen.png"), this.width / 2 - upgradeScreenWidth / 2, this.height / 2 - upgradeScreenHeight / 2, 0, 0, upgradeScreenWidth, upgradeScreenHeight, upgradeScreenWidth, upgradeScreenHeight);
+
+        int screenWidthStart = this.width / 2 - upgradeScreenWidth / 2;
+        int screenHeightStart = this.height / 2 - upgradeScreenHeight / 2;
+
         int level = ClientAttributesData.getValue("Level");
         String levelText = Component.translatable("gui.tponr.level").getString() + " " + level;
-        pGuiGraphics.drawString(this.font, levelText, this.width / 4 - font.width(levelText) / 2, this.height / 10, 0x11FC67, false);
+        pGuiGraphics.drawString(this.font, levelText, screenWidthStart + 36, screenHeightStart + 21, 0x11FC67, false);
 
         int neededXP = 807 + 50 * (level - 1);
 
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
         String expText = "EXP: " + player.totalExperience + " / " + neededXP;
-        pGuiGraphics.drawString(this.font, expText, this.width / 4 - font.width(expText) / 6 * 5, this.height / 8, 0x252726, false);
+        pGuiGraphics.drawString(this.font, expText,  (int) ((screenWidthStart + 26) / 0.9F), (int) ((screenHeightStart + 41) / 0.9F), 0x252726, false);
+        pGuiGraphics.pose().popPose();
 
         String statsText = Component.translatable("gui.tponr.stats").getString();
-        pGuiGraphics.drawString(this.font, statsText, this.width / 4 - font.width(statsText) / 2, this.height / 6, 0x11FC67, false);
+        pGuiGraphics.drawString(this.font, statsText, screenWidthStart + 36, screenHeightStart + 62, 0x11FC67, false);
 
         String healthText = Component.translatable("stat.tponr.health").getString();
-        pGuiGraphics.drawString(this.font, healthText, this.width / 4 - font.width(healthText) / 2, this.height / 2 + 15, 0x11FC67, false);
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
+        pGuiGraphics.drawString(this.font, healthText, (int) ((screenWidthStart + 38) / 0.9F), (int) ((screenHeightStart + 92) / 0.9F), 0xF6F6F6, false);
+        pGuiGraphics.pose().popPose();
+
+        String staminaText = Component.translatable("stat.tponr.stamina_capacity").getString();
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
+        pGuiGraphics.drawString(this.font, staminaText, (int) ((screenWidthStart + 38) / 0.9F), (int) ((screenHeightStart + 104) / 0.9F), 0xF6F6F6, false);
+        pGuiGraphics.pose().popPose();
+
+        String manaText = Component.translatable("stat.tponr.mana").getString();
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
+        pGuiGraphics.drawString(this.font, "§k" + manaText, (int) ((screenWidthStart + 38) / 0.9F), (int) ((screenHeightStart + 117) / 0.9F), 0xF6F6F6, false);
+        pGuiGraphics.pose().popPose();
+
+
+
+        String currentHealthText = String.valueOf((int) player.getHealth());
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
+        pGuiGraphics.drawString(this.font, currentHealthText, (int) ((screenWidthStart + 94 - font.width(currentHealthText)) / 0.9F), (int) ((screenHeightStart + 92) / 0.9F), 0xF6F6F6, false);
+        pGuiGraphics.pose().popPose();
+
+        AtomicReference<String> currentStaminaText = new AtomicReference<>();
+        player.getCapability(PlayerStaminaProvider.PLAYER_STAMINA).ifPresent(stamina -> {
+            currentStaminaText.set(String.valueOf(stamina.getStamina()));
+        });
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
+        pGuiGraphics.drawString(this.font, currentStaminaText.get(), (int) ((screenWidthStart + 94 - font.width(currentStaminaText.get())) / 0.9F), (int) ((screenHeightStart + 105) / 0.9F), 0xF6F6F6, false);
+        pGuiGraphics.pose().popPose();
+
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().scale(0.9F, 0.9F, 1.0F);
+        pGuiGraphics.drawString(this.font, "???", (int) ((screenWidthStart + 94 - font.width("???")) / 0.9F), (int) ((screenHeightStart + 117) / 0.9F), 0xF6F6F6, false);
+        pGuiGraphics.pose().popPose();
 
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
     }
-
 
     @Override
     public boolean isPauseScreen() {
