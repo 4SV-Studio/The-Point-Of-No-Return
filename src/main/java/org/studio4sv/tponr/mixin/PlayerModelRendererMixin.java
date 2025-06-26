1b014b49a8770f50e4e4c6ff27d0cb90df5deb7d
Lemonnik6484 <lemongamer6484@gmail.com>
2025-06-26 20:08:42 +0200

    Move player model to bottom-left corner

--- src/main/java/org/studio4sv/tponr/mixin/PlayerModelRendererMixin.java
+++ src/main/java/org/studio4sv/tponr/mixin/PlayerModelRendererMixin.java
@@ -1,11 +1,13 @@
 package org.studio4sv.tponr.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import ichttt.mods.firstaid.api.damagesystem.AbstractDamageablePart;
+import ichttt.mods.firstaid.api.damagesystem.AbstractPlayerDamageModel;
 import ichttt.mods.firstaid.client.util.PlayerModelRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,8 +20,36 @@ public class PlayerModelRendererMixin {
     @Final
     private static ResourceLocation HEALTH_RENDER_LOCATION;
 
+    @Shadow
+    private static int getState(AbstractDamageablePart part, boolean fourColors) {
+        return 0; // Dummy
+    }
+
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void modifyLock(CallbackInfo ci) {
         HEALTH_RENDER_LOCATION = ResourceLocation.fromNamespaceAndPath(TPONR.MOD_ID, "textures/gui/firstaid/simple_health.png");
     }
+
+    @Inject(method = "renderPlayerHealth", at = @At("HEAD"), remap = false)
+    private static void onRenderHead(PoseStack stack, AbstractPlayerDamageModel damageModel, boolean fourColors, GuiGraphics guiGraphics, boolean flashState, float alpha, float partialTicks, CallbackInfo ci) {
+        stack.pushPose();
+        int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+        stack.translate(0, screenHeight - 64, 0);
+    }
+
+    @Inject(method = "renderPlayerHealth", at = @At("RETURN"), remap = false)
+    private static void onRenderReturn(PoseStack stack, AbstractPlayerDamageModel damageModel, boolean fourColors, GuiGraphics guiGraphics, boolean flashState, float alpha, float partialTicks, CallbackInfo ci) {
+        stack.popPose();
+    }
+
+   /**
+    * @author Lemonnik
+    * @reason Adjust UV for custom texture
+    */
+   @Overwrite
+   private static void drawPart(GuiGraphics guiGraphics, boolean fourColors, AbstractDamageablePart part, int texX, int texY, int sizeX, int sizeY) {
+       int rawTexX = texX;
+       texX += 32 * getState(part, fourColors);
+       guiGraphics.blit(HEALTH_RENDER_LOCATION, rawTexX, texY, texX, texY, sizeX, sizeY);
+   }
 }
