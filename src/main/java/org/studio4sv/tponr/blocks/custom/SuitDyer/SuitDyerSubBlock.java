0cb72239fefcc9a9b22c96f67ca5d516adb81b54
lemonnik <lemongamer6484@gmail.com>
2025-07-23 00:20:08 +0200

    Suit Dyer pack insertion

--- src/main/java/org/studio4sv/tponr/blocks/custom/SuitDyer/SuitDyerSubBlock.java
+++ src/main/java/org/studio4sv/tponr/blocks/custom/SuitDyer/SuitDyerSubBlock.java
@@ -4,13 +4,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
+import org.studio4sv.tponr.blocks.entity.SuitDyer.SuitDyerBlockEntity;
 import org.studio4sv.tponr.blocks.entity.SuitDyer.SuitDyerSubBlockEntity;
+import org.studio4sv.tponr.items.HazmatSuitPackItem;
 
 public class SuitDyerSubBlock extends BaseEntityBlock {
     public SuitDyerSubBlock(Properties pProperties) {
@@ -44,6 +47,24 @@ public class SuitDyerSubBlock extends BaseEntityBlock {
 
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+        if (!pLevel.isClientSide) {
+            if (pPlayer.getItemInHand(pHand).getItem() instanceof HazmatSuitPackItem) {
+                BlockEntity subBlockEntity = pLevel.getBlockEntity(pPos);
+                if (subBlockEntity instanceof SuitDyerSubBlockEntity) {
+                    if (pLevel.getBlockEntity(((SuitDyerSubBlockEntity) subBlockEntity).getMainBlockPos()) instanceof SuitDyerBlockEntity mainBlockEntity) {
+                        mainBlockEntity.setStoredItem(pPlayer.getItemInHand(pHand).copyAndClear());
+                    }
+                }
+            } else if (pPlayer.getItemInHand(pHand).isEmpty()) {
+                BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+                if (blockEntity instanceof SuitDyerSubBlockEntity) {
+                    if (pLevel.getBlockEntity(((SuitDyerSubBlockEntity) blockEntity).getMainBlockPos()) instanceof SuitDyerBlockEntity mainBlockEntity) {
+                        pPlayer.setItemInHand(pHand, mainBlockEntity.getStoredItem());
+                        mainBlockEntity.setStoredItem(ItemStack.EMPTY);
+                    }
+                }
+            }
+        }
+        return InteractionResult.SUCCESS;
     }
 }
--- src/main/java/org/studio4sv/tponr/blocks/entity/SuitDyer/SuitDyerBlockEntity.java
+++ src/main/java/org/studio4sv/tponr/blocks/entity/SuitDyer/SuitDyerBlockEntity.java
@@ -1,9 +1,14 @@
 package org.studio4sv.tponr.blocks.entity.SuitDyer;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.studio4sv.tponr.armor.HazmatSuitItem;
+import org.studio4sv.tponr.items.HazmatSuitPackItem;
 import org.studio4sv.tponr.registers.ModBlockEntities;
+import org.studio4sv.tponr.registers.ModItems;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
@@ -13,10 +18,57 @@ import software.bernie.geckolib.util.RenderUtils;
 
 public class SuitDyerBlockEntity extends BlockEntity implements GeoBlockEntity {
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private ItemStack storedItem = ItemStack.EMPTY;
 
     public SuitDyerBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.SUIT_DYER_ENTITY.get(), pPos, pBlockState);
     }
+
+    public void setStoredItem(ItemStack stack) {
+        this.storedItem = stack;
+        setChanged();
+    }
+
+    public ItemStack getStoredItem() {
+        return storedItem;
+    }
+
+    public ItemStack[] getStoredArmor() {
+
+        if (!(storedItem.getItem() instanceof HazmatSuitPackItem)) {
+            return new ItemStack[] {
+                    ItemStack.EMPTY, ItemStack.EMPTY, ItemStack.EMPTY, ItemStack.EMPTY
+            };
+        }
+
+        CompoundTag tag = storedItem.getOrCreateTag();
+        int charge = tag.getInt("charge");
+        int color;
+
+        if (storedItem.getItem() instanceof HazmatSuitPackItem dyeable) {
+            color = dyeable.getColor(storedItem);
+        } else {
+            color = 0xFFFFFF;
+        }
+
+
+        ItemStack helmet = new ItemStack(ModItems.HAZMAT_SUIT_HELMET.get());
+        ItemStack chest = new ItemStack(ModItems.HAZMAT_SUIT_CHESTPLATE.get());
+        ItemStack legs = new ItemStack(ModItems.HAZMAT_SUIT_LEGGINGS.get());
+        ItemStack boots = new ItemStack(ModItems.HAZMAT_SUIT_BOOTS.get());
+
+        for (ItemStack piece : new ItemStack[]{helmet, chest, legs, boots}) {
+            CompoundTag pieceTag = piece.getOrCreateTag();
+            if (piece.getItem() instanceof HazmatSuitItem dyeable) {
+                dyeable.setColor(piece, color);
+            }
+            pieceTag.putInt("charge", charge);
+        }
+
+        return new ItemStack[] {helmet, chest, legs, boots};
+    }
+
+
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
@@ -35,6 +87,52 @@ public class SuitDyerBlockEntity extends BlockEntity implements GeoBlockEntity {
     @Override
     public double getTick(Object blockEntity) {
         return RenderUtils.getCurrentTick();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+
+        CompoundTag itemTag = new CompoundTag();
+        storedItem.save(itemTag);
+        pTag.put("StoredItem", itemTag);
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+
+        if (pTag.contains("StoredItem")) {
+            storedItem = ItemStack.of(pTag.getCompound("StoredItem"));
+        }
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        CompoundTag tag = super.getUpdateTag();
+
+        CompoundTag itemTag = new CompoundTag();
+        storedItem.save(itemTag);
+        tag.put("StoredItem", itemTag);
+        return tag;
+    }
+
+    @Override
+    public void handleUpdateTag(CompoundTag pTag) {
+        super.handleUpdateTag(pTag);
+
+        if (pTag.contains("StoredItem")) {
+            storedItem = ItemStack.of(pTag.getCompound("StoredItem"));
+        }
+    }
+
+    public boolean setColor(int color) {
+        if (storedItem.getItem() instanceof HazmatSuitPackItem dyeable) {
+            dyeable.setColor(storedItem, color);
+            setChanged();
+            return true;
+        }
+        return false;
     }
 }
 
