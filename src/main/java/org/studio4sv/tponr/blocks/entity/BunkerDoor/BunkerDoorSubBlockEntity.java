311493a0de2e0dd8d6c25d6f764213eb751ee786
lemonnik <lemongamer6484@gmail.com>
2025-08-20 13:53:46 +0200

    Fix

--- src/main/java/org/studio4sv/tponr/blocks/entity/BunkerDoor/BunkerDoorSubBlockEntity.java
+++ src/main/java/org/studio4sv/tponr/blocks/entity/BunkerDoor/BunkerDoorSubBlockEntity.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.IntTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.studio4sv.tponr.registers.ModBlockEntities;
@@ -27,35 +28,24 @@ public class BunkerDoorSubBlockEntity extends BlockEntity {
     @Override
     protected void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
-        ListTag list = new ListTag();
         if (mainBlockPos != null) {
-            list.add(IntTag.valueOf(mainBlockPos.getX()));
-            list.add(IntTag.valueOf(mainBlockPos.getY()));
-            list.add(IntTag.valueOf(mainBlockPos.getZ()));
-            tag.put("MainBlockPos", list);
+            tag.put("MainBlockPos", NbtUtils.writeBlockPos(mainBlockPos));
         }
     }
 
     @Override
     public void load(CompoundTag tag) {
         super.load(tag);
-        if (tag.contains("MainBlockPos", ListTag.TAG_LIST)) {
-            ListTag list = tag.getList("MainBlockPos", ListTag.TAG_LIST);
-            if (list.size() == 3) {
-                mainBlockPos = new BlockPos(list.getInt(0), list.getInt(1), list.getInt(2));
-            }
+        if (tag.contains("MainBlockPos")) {
+            mainBlockPos = NbtUtils.readBlockPos(tag.getCompound("MainBlockPos"));
         }
     }
 
     @Override
     public CompoundTag getUpdateTag() {
         CompoundTag tag = super.getUpdateTag();
-        ListTag list = new ListTag();
         if (mainBlockPos != null) {
-            list.add(IntTag.valueOf(mainBlockPos.getX()));
-            list.add(IntTag.valueOf(mainBlockPos.getY()));
-            list.add(IntTag.valueOf(mainBlockPos.getZ()));
-            tag.put("MainBlockPos", list);
+            tag.put("MainBlockPos", NbtUtils.writeBlockPos(mainBlockPos));
         }
         return tag;
     }
@@ -63,11 +53,8 @@ public class BunkerDoorSubBlockEntity extends BlockEntity {
     @Override
     public void handleUpdateTag(CompoundTag tag) {
         super.handleUpdateTag(tag);
-        if (tag.contains("MainBlockPos", ListTag.TAG_LIST)) {
-            ListTag list = tag.getList("MainBlockPos", ListTag.TAG_LIST);
-            if (list.size() == 3) {
-                mainBlockPos = new BlockPos(list.getInt(0), list.getInt(1), list.getInt(2));
-            }
+        if (tag.contains("MainBlockPos")) {
+            mainBlockPos = NbtUtils.readBlockPos(tag.getCompound("MainBlockPos"));
         }
     }
 }
