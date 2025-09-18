ddfcb254c2521a1a753a804d9ebcabac91615525
lemonnik <lemongamer6484@gmail.com>
2025-09-18 18:35:53 +0200

    armor util

--- src/main/java/org/studio4sv/tponr/registers/ModItems.java
+++ src/main/java/org/studio4sv/tponr/registers/ModItems.java
@@ -9,7 +9,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.studio4sv.tponr.util.ModArmorMaterials;
+import org.studio4sv.tponr.util.armor.ModArmorMaterials;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
