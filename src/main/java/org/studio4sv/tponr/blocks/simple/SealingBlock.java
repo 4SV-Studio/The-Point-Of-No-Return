f72496f56b901fed284e439abafc1c34a2710d87
lemonnik <lemongamer6484@gmail.com>
2025-09-18 18:37:04 +0200

    Seal block

---
+++ src/main/java/org/studio4sv/tponr/blocks/simple/SealingBlock.java
@@ -0,0 +1,9 @@
+package org.studio4sv.tponr.blocks.simple;
+
+import net.minecraft.world.level.block.Block;
+
+public class SealingBlock extends Block {
+    public SealingBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
--- src/main/java/org/studio4sv/tponr/registers/ModBlocks.java
+++ src/main/java/org/studio4sv/tponr/registers/ModBlocks.java
@@ -53,6 +53,10 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.BEDROCK))
     );
 
+    public static final RegistryObject<Block> SEALING_BLOCK = BLOCKS.register("sealing_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK))
+    );
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
